aad26effc6c7f4338e5ef20f4afe77adb6f90953
Dhika <putraadhika15@gmail.com>
2024-07-02 19:07:08 +0700

    add approver

---
+++ app/Http/Controllers/API/ApproverController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ApproverRequest;
+use App\Http\Resources\ApproverResource;
+use App\Repositories\ApproverRepository;
+
+/**
+*    @OA\Post(
+*       path="/approvers",
+*       tags={"Approver"},
+*       @OA\Response(
+*           response="200",
+*           description="Ok",
+*           @OA\JsonContent
+*           (example={
+*               "success": true,
+*               "message": "Berhasil mengambil Kategori Berita",
+*               "data": {
+*                   {
+*                   "id": "1",
+*                   "nama_kategori": "Pendidikan",
+*                  }
+*              }
+*          }),
+*      ),
+*  )
+*/
+class ApproverController extends Controller
+{
+    protected $approver;
+
+    public function __construct(ApproverRepository $approver) {
+        $this->approver = $approver;
+    }
+
+    public function store(ApproverRequest $request)
+    {
+        try {
+            $store = $this->approver->store($request->approve());
+
+            return new ApproverResource($store);
+
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ApproverController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/approvers', [ApproverController::class,'store']);
